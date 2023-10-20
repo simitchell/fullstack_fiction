528a0d53c806eca6f606d52d236dc18562a6b225
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-20 14:54:15 -0400

    installed bootstrap and setuptools

--- fullstack_framework/urls.py
+++ fullstack_framework/urls.py
@@ -26,4 +26,10 @@ router.register(r'books', views.BookViewSet)
 urlpatterns = [
     path('', include (router.urls)),
     path('admin/', admin.site.urls),
+    path('token/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name ='token_refresh')
 ]
